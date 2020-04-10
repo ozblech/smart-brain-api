4cd55fda01cb92b848ee58f5e5f1e5b575d1e15f
ozblech <ozblech87@gmail.com>
2020-04-10 18:20:06 +0300

    fixed some error

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(express.json());
 app.use(cors())
 
 
-// app.get('/', (req, res) => {
-// 	res.send(database.users)
-// })
+app.get('/', (req, res) => {
+	res.send('It is working')
+})
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
